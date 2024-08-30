79139be4beca6603659d54893860725e31ab268e
Jorge O. Castro <jorge.castro@gmail.com>
2024-08-29 21:37:26 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,8 +13,20 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install adobe-source-code-pro-fonts android-tools bpftop cascadia-code-fonts containererd.io \
+edk2-ovmf flatpak-builder genisoimage iotop kcli libvirt libvirt-nss p7zip-plugins p7zip podman-compose \
+podman-tui podmansh powertop qemu-char-spice qemu-device-display-virtio-gpu qemu-device-display-virtio-vga \
+qemu-device-usb-redirect qemu-img qemu-system-x86-core qemu-user-binfmt qemu-user-static qemu rocm-hip \
+rocm-opencl rocm-smi ubuntu-family-fonts udica umoci virt-manager virt-viewer ydotool dbus-x11 
 
+#				"docker-ce",
+#				"docker-ce-cli",
+#				"docker-buildx-plugin",
+#				"docker-compose-plugin",
+#				"edk2-ovmf",
+#				"flatpak-builder",
+# devpod and fonts are also missing				
+    
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
