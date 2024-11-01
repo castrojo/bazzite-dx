ec0f6e6e71ddf44b957f25e1b58728842ff79397
Jorge O. Castro <jorge.castro@gmail.com>
2024-10-31 23:21:37 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+dnf install dnf-plugins-core
 dnf copr enable ublue-os/akmods
 dnf install jupiter
 
