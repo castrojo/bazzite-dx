cd9dad8be4f3fd90c4f07d3acdee19ee4610e47c
Jorge O. Castro <jorge.castro@gmail.com>
2024-11-21 10:03:07 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ dnf5 install -y dnf-plugins-core
 dnf5 copr enable ublue-os/akmods
 
 # this installs a package from fedora repos
-dnf5 install -y iotop
+dnf5 install iotop
 
 #				"docker-ce",
 #				"docker-ce-cli",
