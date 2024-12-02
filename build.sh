071b0a3578f81b0af2e95ef7b8fac96a2c3714be
Jorge O. Castro <jorge.castro@gmail.com>
2024-12-02 18:44:38 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 dnf5 copr enable -y ublue-os/akmods
-dnf5 group install -y domain-client
+dnf5 group install -y domain-client cosmic-desktop cosmic-desktop-apps
 
 
 #### Example for enabling a System Unit File
