411cbc71eece7fc0d065ed56caeaf36dd1b11f25
James Reilly <jreilly1821@gmail.com>
2024-12-17 12:04:30 +0530

    No gnome-tweaks available by default in centos

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -ouex pipefail
 
 dnf group install -y --nobest Workstation
 
-dnf install -y \
-	gnome-tweaks \
-	gnome-extensions-app \
-	gnome-shell-extension-appindicator \
+# dnf install -y \
+#	gnome-tweaks \
+#	gnome-extensions-app \
+#	gnome-shell-extension-appindicator \
 
  systemctl enable \
     gdm.service
