0cc01c354fa3100291984a7fa4b3e5746247e384
Jorge O. Castro <jorge.castro@gmail.com>
2024-12-16 22:13:26 -0500

    Update build.sh
    Thanks heliumos: https://codeberg.org/HeliumOS/bootc/src/commit/c33dc404360a0f58d7771534f0071a3204196643/9/desktop.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,15 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 
-dnf group install -y Workstation
+dnf group install -y --nobest @Workstation
+
+dnf install -y \
+	gnome-tweaks \
+	gnome-extensions-app \
+	gnome-shell-extension-appindicator \
+
+ systemctl enable \
+    gdm.service
 
 
 #### Example for enabling a System Unit File
