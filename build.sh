f00867c5903e6861f2364d186d763f508151675d
Tulip Blossom <tulilirockz@outlook.com>
2024-12-18 12:46:10 -0300

    feat: brew on build!

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ dnf install -y \
     gnome-extensions-app \
     gnome-shell-extension-appindicator \
     gnome-shell-extension-dash-to-dock
+
+# install homebrew like bluefin does :)
+curl --retry 3 -Lo /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+chmod +x /tmp/brew-install
+/tmp/brew-install
+tar --zstd -cvf /usr/share/homebrew.tar.zst /home/linuxbrew/.linuxbrew
